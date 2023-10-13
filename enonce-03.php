baae74a9ecfc0b94c40a629b000fa8ca0dc3e466
Mohamed WLaptop <114567817+MDDiallo9@users.noreply.github.com>
2023-10-13 10:40:07 +0200

    fini

--- enonce-03.php
+++ enonce-03.php
@@ -1,25 +1,26 @@
-<?php 
-    function debug($value)
-    {
-        echo '<pre style="background-color:black;color:white;overflow: auto;padding: 1rem;font-family:monospace;">';
-        print_r($value);
-        // var_dump($value);
-        echo '</pre>';
-    }
-    
-    $path = 'data/colors.json';
-    $jsonString = file_get_contents($path);
-    $jsonData = json_decode($jsonString, true);
-    
+<?php
+function debug($value)
+{
+    echo '<pre style="background-color:black;color:white;overflow: auto;padding: 1rem;font-family:monospace;">';
+    print_r($value);
+    // var_dump($value);
+    echo '</pre>';
+}
+
+$path = 'data/colors.json';
+$jsonString = file_get_contents($path);
+$jsonData = json_decode($jsonString, true);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        .box{
+        .box {
             height: 100px;
             width: 100px;
             color: white;
@@ -29,13 +30,15 @@
         }
     </style>
 </head>
+
 <body>
-    <?php 
-    foreach($jsonData as $color){
+    <?php
+    foreach ($jsonData as $color) {
     ?>
-    <div class="box" style="background-color:<?= $color["value"] ?> ;">
-        <?= $color["color"] ?>
-    </div>
-    <?php }?>
+        <div class="box" style="background-color:<?= $color["value"] ?> ;">
+            <?= $color["color"] ?>
+        </div>
+    <?php } ?>
 </body>
+
 </html>
