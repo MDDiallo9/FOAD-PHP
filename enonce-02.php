5d631f0b093a11a2ae31649321cb063a6e294c37
Mohamed WLaptop <114567817+MDDiallo9@users.noreply.github.com>
2023-10-13 10:11:52 +0200

    ajout de la fonction debug pour plus de lisibilité

--- enonce-02.php
+++ enonce-02.php
@@ -1,6 +1,17 @@
 <?php 
+ 
+ function debug($value)
+ {
+     echo '<pre style="background-color:black;color:white;overflow: auto;padding: 1rem;font-family:monospace;">';
+     print_r($value);
+     // var_dump($value);
+     echo '</pre>';
+ }
+
 $path = 'data/persons.json';
 $jsonString = file_get_contents($path);
 $jsonData = json_decode($jsonString, true);
-var_dump($jsonData);
+debug($jsonData);
+
+
 ?>
